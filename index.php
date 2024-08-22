572c7d168b16a60e7f2d62e6a064092a121339b2
Mariana Marca <mariana.marca.herrera.2003@gmail.com>
2024-08-22 15:09:06 -0400

    reparacion de cliente

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 /*controladores*/
 require_once "controlador/plantillaControlador.php";
 require_once "controlador/usuarioControlador.php";
+require_once "controlador/clienteControlador.php";
 
 
 require_once "modelo/usuarioModelo.php";
+require_once "modelo/clienteModelo.php";
 
 $plantilla=new controladorPlantilla();
 $plantilla->ctrPlantilla();
